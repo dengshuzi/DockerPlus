404aa60306ca223aa73e7218f3c1446f75cfc9b9
jsJuusan <1402553354@qq.com>
2023-09-09 00:21:04 +0800

    2023/9/9

---
+++ DockerPlus/src/main/java/cn/com/dockerplus/DockerPlus/DockerPlusApplication.java
@@ -0,0 +1,13 @@
+package cn.com.dockerplus.DockerPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPlusApplication.class, args);
+	}
+
+}
---
+++ DockerPlus/src/test/java/cn/com/dockerplus/DockerPlus/DockerPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dockerplus.DockerPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
